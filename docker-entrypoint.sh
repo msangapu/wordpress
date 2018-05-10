d0a17cdd7aa3a37c30918458b7328c05c642f578
Mangesh Sangapu <msangapu@microsoft.com>
2018-05-10 15:19:30 -0500

    app setting changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -159,8 +159,8 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 		if [ ! -e wp-config.php ]; then
 			awk '/^\/\*.*stop editing.*\*\/$/ && c == 0 { c = 1; system("cat") } { print }' wp-config-sample.php > wp-config.php <<'EOPHP'
 
-define('WP_REDIS_HOST', 'redis');
-define('MYSQL_SSL_CA', 'BaltimoreCyberTrustRoot.crt.pem');
+define('WP_REDIS_HOST', getenv('WP_REDIS_HOST'));
+define('MYSQL_SSL_CA', getenv('MYSQL_SSL_CA'));
 define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL | MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 
 // If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
